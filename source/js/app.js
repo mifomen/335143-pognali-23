9b32fa22f990be6f02dbd7a05b3956804ed5b10f
Ilya <mifomen@yandex.ru>
2021-07-26 00:19:58 +0300

    style for index.html

--- source/js/app.js
+++ source/js/app.js
@@ -1,1 +1,17 @@
-console.log('work')
+console.log("work")
+let map;  // ,
+const coordinates = {  lat: 59.938805, lng: 30.323259};
+function initMap() {
+  map = new google.maps.Map(document.getElementById("address__map-block-google"), {
+    center: { lat: 59.938635, lng: 30.323118},
+    zoom: 18,
+  });
+  new google.maps.Marker({
+    position: coordinates,
+    map,
+    title: "Мы здесь!",
+    // content:"<h1 class="blog-content-link">mifomen</h1>",
+    icon: "img/favicon/16.png",
+    draggable: false
+  });
+}
